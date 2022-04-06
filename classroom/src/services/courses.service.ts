b0f76e2d2957871e065adc07257843be14820c75
Renan Pereira Loureiro <renanpl47@gmail.com>
2022-04-05 22:22:21 -0300

    feat: list all students

---
+++ classroom/src/services/courses.service.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma/prisma.service';
+@Injectable()
+export class CoursesService {
+  constructor(private prisma: PrismaService) {}
+}
